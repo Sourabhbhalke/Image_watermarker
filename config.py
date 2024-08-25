1f2cb8e2b7b5cad4b105bdc2034eaf473c18c79a
Sourabhbhalke <138807690+Sourabhbhalke@users.noreply.github.com>
2024-08-25 02:44:11 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,50 @@
+import os
+from os import environ
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    SECRET_KEY = 'SourabhB'
+
+    DB_NAME = "production-db"
+    DB_USERNAME = "root"
+    DB_PASSWORD = "SourabhB"
+
+    UPLOADS = "/home/username/app/app/static/uploads"
+
+    SESSION_COOKIE_SECURE = True
+    DEFAULT_THEME = None
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+    DB_NAME = "development-db"  # Assuming this should be for development
+    DB_USERNAME = "root"
+    DB_PASSWORD = "SourabhB"
+
+    UPLOADS = "/home/username/app/app/static/uploads"
+    SESSION_COOKIE_SECURE = False
+
+
+class TestingConfig(Config):
+    DEBUG = True
+
+    DB_NAME = "testing-db"  # Assuming this should be for testing
+    DB_USERNAME = "root"
+    DB_PASSWORD = "SourabhB"
+
+    UPLOADS = "/home/username/app/app/static/uploads"
+    SESSION_COOKIE_SECURE = False
+
+
+class DebugConfig(Config):
+    DEBUG = False
+
